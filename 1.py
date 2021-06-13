2679272f66e25be610bf83c3e0e56882acd2efdf
Rozhin-sharifi <80633561+Rozhin-sharifi@users.noreply.github.com>
2021-06-13 12:26:49 +0430

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ y=160
 image=np.zeros((x,y))
 
 for i in range (0,x,20):
-        if i%2==0:
+        if (i/20)%2==0:
             for j in range (0,y,20):
                 if j%2==0:
                     image[i:i+20,j:j+20]=np.zeros((20,20))
@@ -15,9 +15,9 @@ for i in range (0,x,20):
                     image[i:i+20,j:j+20]=np.ones((20,20))*255
         else:
             for j in range (0,y,20):
-                if j%2==0:
-                    image[i:i+20,j:j+20]=np.zeros((20,20))
-                else:
+                if (j/20)%2==0:
                     image[i:i+20,j:j+20]=np.ones((20,20))*255
+                else:
+                    image[i:i+20,j:j+20]=np.zeros((20,20))
 cv2.imshow('Picture',image)
 cv2.waitKey()
